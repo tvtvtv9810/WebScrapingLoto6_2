faabbb7d049a54e720168963e3edd399bcc52906
tvtvtv9810 <tvtvtv9810@gmail.com>
2023-02-04 16:47:48 +0900

    不具合など修正

--- python/src/scraping_urlLib.py
+++ python/src/scraping_urlLib.py
@@ -13,11 +13,13 @@ from selenium.webdriver.support import expected_conditions as EC
 # ロト6の当選番号が掲載されているみずほ銀行ページのURL
 loto_url1 = 'https://www.mizuhobank.co.jp/retail/takarakuji/loto/backnumber/loto6'  # 1～460回目
 loto_url2 = 'https://www.mizuhobank.co.jp/retail/takarakuji/loto/backnumber/detail.html?fromto='  # 461回目以降
-num = 1
+startNum = 1  # 461
+num = startNum
 
+times_list = []  # 第何回目かリスト
+lottery_date_list = []  # 抽選日リスト
 main_num_list = []  # 本数字6桁を格納するリスト
 bonus_num_list = []  # ボーナス数字を格納するリスト
-
 
 # PhantomJSをselenium経由で利用
 # driver = webdriver.PhantomJS()
@@ -42,25 +44,9 @@ driver = webdriver.Chrome('chromedriver', options=options)
 
 driver.implicitly_wait(10)  # seconds
 
-# 実行したいスクリプト
-java_script = '\
-    function jquery_exe(){\
-        /*\
-        console.log("test");\
-        */\
-    };\
-    function require(uri) {\
-        var js = document.createElement("script");\
-        js.type = "text/javascript";\
-        js.src = uri + "?v=" + new Date().getTime().toString();\
-        var head = document.getElementsByTagName("head")[0];\
-        head.appendChild(js);\
-    };\
-    require("http://code.jquery.com/jquery-3.4.1.js");\
-    setTimeout(jquery_exe, 1000);\
-'
+isFirst = True
 
-while num <= 1341:
+while num <= 1651:
 
     # 第1～460回目までの当選ページのURL
     if num < 461:
@@ -71,11 +57,11 @@ while num <= 1341:
 
     # PhntomJSで該当ページを取得
     driver.get(url)
-    print("- driver -----------------------------------")
-    print(driver)
-    print("------------------------------------")
-    # スクリプト実行
-    # driver.execute_script(java_script)
+    if isFirst:
+        print("初回：10秒待機して再度・・・")
+        time.sleep(10)
+        driver.get(url)
+        isFirst = False
 
     # time.sleep(2) # javascriptのページを読み込む時間
     # 途中から取得先のサイトが非同期になってるため、遅延時間を変更
@@ -85,30 +71,36 @@ while num <= 1341:
     #     EC.presence_of_element_located((By.CLASS_NAME, "wdt33p")))
     html = driver.page_source.encode('utf-8')
     soup = BeautifulSoup(html, "html.parser")
-    print("- soup -----------------------------------")
-    print(soup)
-    print("------------------------------------")
 
     print(soup.title)
 
     # ロト6の当選番号がのっているテーブルの取得
-    table = soup.find_all("table")
-    del table[0]
+    tables = soup.find_all("table")
 
-    for i in table:
+    # 1つ目は表示用？２つめ以降は内部データ？1tableで1回分っぽい
+    del tables[0]  # １つめ削除
+
+    for table1Dose in tables:
+        tBody = table1Dose.find("tbody")
+        trList = tBody.find_all("tr")
+
+        # 第何回目
+        times_list.append(trList[0].find("td").string)
+        # 抽選日リスト
+        lottery_date_list.append(trList[1].find("td").string)
+
         # 本数字の取得
-        main_num = i.find_all("tr")[2].find("td")
+        main_nums_str = trList[2].find("td").string
         print("------------------------------------")
-        print(main_num)
-        print(main_num.string)
+        print(main_nums_str)
         print("------------------------------------")
-        if main_num.string == None:
-            print("continue;;;;")
-            continue
-        main_num_list.append(main_num.string.split(" "))
+        if main_nums_str != None:
+            main_num_list.append(main_nums_str.split(" "))
+        else:
+            main_num_list.append(["-", "-", "-", "-", "-", "-"])
 
         # ボーナス数字の取得
-        bonus_num = i.find_all("tr")[3].find("td")
+        bonus_num = trList[3].find("td")
         bonus_num_list.append(bonus_num.string)
 
     num += 20  # 次のページに移動するためにnumに20を追加
@@ -118,5 +110,7 @@ while num <= 1341:
 df = pd.DataFrame(main_num_list, columns=[
     'main1', 'main2', 'main3', 'main4', 'main5', 'main6'])
 df['bonus'] = bonus_num_list
+df['times'] = times_list
+df['date'] = lottery_date_list
 df.index = df.index + 1
 df.to_csv('loto6.csv')
